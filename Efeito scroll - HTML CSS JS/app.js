98d3736459302f1ec87be5de575d863f20f20391
Domingos Pedro Nkula <72228890+domingospedro-dev@users.noreply.github.com>
2021-01-19 19:03:45 +0100

    Otimizando o meu código Js.
    Nesta versão otimizei, ou seja, simplifiquei o meu código Js, mas não perdeu o resultado que tinha. Apenas tornei o código mais simples e mais produtivo.

--- Efeito scroll - HTML CSS JS/app.js
+++ Efeito scroll - HTML CSS JS/app.js
@@ -1,111 +1,52 @@
 var box = document.getElementsByClassName("treino");
 
-box[0].style.backgroundColor = "#f12f2f";
-box[1].style.backgroundColor = "#f1cd2f";
-box[2].style.backgroundColor = "#86f12f";
-box[3].style.backgroundColor = "#2fc4f1";
-box[4].style.backgroundColor = "#f12fb0";
-box[5].style.backgroundColor = "#3647e7";
+// ESTILIZANDO
+for (i = 0; i < box.length; i++) {
 
-
-box[0].style.opacity = ".5"
-box[0].style.transform = "translateX(-10em)"
-box[0].style.transition = ".5s"
-
-
-box[1].style.opacity = ".5"
-box[1].style.transform = "translateX(10em)"
-box[1].style.transition = ".5s"
-
-box[2].style.opacity = ".5"
-box[2].style.transform = "translateX(-10em)"
-box[2].style.transition = ".5s"
-
-
-box[3].style.opacity = ".5"
-box[3].style.transform = "translateX(10em)"
-box[3].style.transition = ".5s"
-
-box[4].style.opacity = ".5"
-box[4].style.transform = "translateX(10em)"
-box[4].style.transition = ".5s"
-
-box[5].style.opacity = ".5"
-box[5].style.transform = "translateX(10em)"
-box[5].style.transition = ".5s"
-
-window.addEventListener("scroll", function () {
-
-
-    var tamTela = window.innerHeight
-    var tela = tamTela / 3
-
-    var tam0 = box[0].getBoundingClientRect();
-    var tam1 = box[1].getBoundingClientRect();
-    var tam2 = box[2].getBoundingClientRect();
-    var tam3 = box[3].getBoundingClientRect();
-    var tam4 = box[4].getBoundingClientRect();
-    var tam5 = box[5].getBoundingClientRect();
-
-    if (tam0.top < tela * 2) {
-        box[0].style.opacity = "1"
-        box[0].style.transform = "translateX(0)"
+    if (i % 2 == 0) {
+        box[i].style.opacity = ".5";
+        box[i].style.transform = "translateX(-10em)";
+        box[i].style.transition = ".5s";
     } else {
-        box[0].style.opacity = ".5"
-        box[0].style.transform = "translateX(-10em)"
+        box[i].style.opacity = ".5";
+        box[i].style.transform = "translateX(10em)";
+        box[i].style.transition = ".5s";
+    }
+}
+
+
+window.addEventListener("scroll", () => {
+
+    var tamanhos = [];
+
+    for (i = 0; i < box.length; i++) {
+        tamanhos.push(box[i].getBoundingClientRect().top);
     }
 
-    // -------------
+    var tamTela = window.innerHeight;
+    var tela = tamTela / 3;
 
-    if (tam1.top < tela * 2) {
-        box[1].style.opacity = "1"
-        box[1].style.transform = "translateX(0)"
-    } else {
-        box[1].style.opacity = ".5"
-        box[1].style.transform = "translateX(10em)"
+
+    for (var i = 0; i < box.length; i++) {
+
+        if (i % 2 == 0) {
+
+            if (tamanhos[i] < tela * 2) {
+                box[i].style.opacity = "1";
+                box[i].style.transform = "translateX(0)";
+            } else {
+                box[i].style.opacity = ".5";
+                box[i].style.transform = "translateX(-10em)";
+            }
+        } else {
+
+            if (tamanhos[i] < tela * 2) {
+                box[i].style.opacity = "1";
+                box[i].style.transform = "translateX(0)";
+            } else {
+                box[i].style.opacity = ".5"
+                box[i].style.transform = "translateX(10em)";
+            }
+        }
     }
-
-    // -------------
-
-    if (tam2.top < tela * 2) {
-        box[2].style.opacity = "1"
-        box[2].style.transform = "translateX(0)"
-    } else {
-        box[2].style.opacity = ".5"
-        box[2].style.transform = "translateX(-10em)"
-    }
-
-    // -------------
-
-    if (tam3.top < tela * 2) {
-        box[3].style.opacity = "1"
-        box[3].style.transform = "translateX(0)"
-    } else {
-        box[3].style.opacity = ".5"
-        box[3].style.transform = "translateX(10em)"
-    }
-
-    // -------------
-
-    if (tam4.top < tela * 2) {
-        box[4].style.opacity = "1"
-        box[4].style.transform = "translateX(0)"
-    } else {
-        box[4].style.opacity = ".5"
-        box[4].style.transform = "translateX(-10em)"
-    }
-
-    // -------------
-
-    if (tam5.top < tela * 2) {
-        box[5].style.opacity = "1"
-        box[5].style.transform = "translateX(0)"
-    } else {
-        box[5].style.opacity = ".5"
-        box[5].style.transform = "translateX(10em)"
-    }
-
 })
-
-
-
